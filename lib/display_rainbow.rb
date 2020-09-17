eb98c113ed8b0d73199a8392c2a69b28a5268cf7
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-09-17 19:59:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-colors = [ R = "red", O = "orange", Y = "yellow", G = "green", B = "blue", I = "indigo", V = "violet"]
+colors = [ "red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def display_rainbow(colors)
   puts 
